141bafd5d62e2c158c81fec14b2d5b6de968f344
acebedomarkace <32662746+acebedomarkace@users.noreply.github.com>
2022-12-11 23:45:56 +0800

    Update jody.py

--- jody.py
+++ jody.py
@@ -13,7 +13,7 @@ st.markdown(f"Today is **{today}**")
 
 with st.sidebar:
     st.subheader("Thanks for working with JO!\nThe Joyful Observer's assistant Web App")
-    st.markdown("This app will help you summarize your joyful observations\n Move the sliders and type those comments\n get an automated summary\nHave a great meeting!")
+    st.caption("Have a great meeting!")
 
 
 tab1, tab2, tab3 = st.tabs(["Meeting Areas","Psychological Safety","Summary"])
