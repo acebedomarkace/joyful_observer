d98724fdb6d272577e8477f97bc57ace374ec6a9
acebedomarkace <32662746+acebedomarkace@users.noreply.github.com>
2023-03-20 09:50:39 +0800

    Update jody.py

--- jody.py
+++ jody.py
@@ -23,8 +23,8 @@ st.markdown(f"Today is {today}")
 
 with st.sidebar:
     st.subheader("Thanks for working with JO!\nThe Joyful Observer's assistant Web App")
-    st.write("Please contact us to get access!")
-    button(username="fake-username", floating=True, width=221)
+    # st.write("Please contact us to get access!")
+    # button(username="fake-username", floating=True, width=221)
     
 
 tab1, tab2, tab3 = st.tabs(["Meeting Areas","Psychological Safety","Summary"])
